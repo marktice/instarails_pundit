4ff24f2aed4f1a9b9bd623687d4ad343ad8517ea
Mark Tice <marktice@protonmail.com>
2018-04-20 13:00:57 +1000

    Update image migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420004916) do
+ActiveRecord::Schema.define(version: 20180420024750) do
+
+  create_table "images", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "image_data"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_images_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
